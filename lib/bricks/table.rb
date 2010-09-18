41b7e7294ac0b7d340f22801dbb52e310574dd7b
Lucas Florio <lucasefe@gmail.com>
2010-09-18 14:07:27 -0300

    Table should have rows.

--- lib/bricks/table.rb
+++ lib/bricks/table.rb
@@ -1,5 +1,19 @@
 module Bricks
   class Table
+    attr_accessor :options
     
+    def initialize(*args)      
+      @options = extract_options!(args)      
+      args.first.each do |row|
+        rows << row
+      end unless args.first.nil?
+    end
+    def rows
+      @rows ||= []
+    end
+  private
+    def extract_options!(args)
+      args.last.is_a?(::Hash) ? args.pop : {}
+    end
   end
 end
--- spec/bricks/table_spec.rb
+++ spec/bricks/table_spec.rb
@@ -3,19 +3,28 @@ require "spec_helper"
 describe Bricks::Table do
   before(:each) do
     @simple_data = [
-        [1,2,3]
+        [ 1, "lucas", "lucasefe@hh.com"]
       ]
+    @options = { :header => %w(id name email) }
   end
   it "should be initializable with no arguments" do
-    lambda { 
-      table = Bricks::Table.new
-    }.should_not raise_error
+    table = Bricks::Table.new
+    table.rows.should be_empty
+    table.options.should == {}
   end
-  context "being initialized with data" do
+  context "being initialized" do
     it "should be initializable with and array of arrays" do
-      lambda { 
-        table = Bricks::Table.new(@simple_data)
-      }.should_not raise_error
-    end    
+      table = Bricks::Table.new(@simple_data)
+      table.rows[0].should == @simple_data[0]
+    end
+    it "should be able to receive only options, with no data" do
+      table = Bricks::Table.new(@options)
+      table.options.should == @options
+    end
+    it "should be able to receive data and options" do
+      table = Bricks::Table.new(@simple_data, @options)
+      table.rows[0].should == @simple_data[0]
+      table.options.should == @options
+    end
   end
 end
