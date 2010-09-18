9d8cdfac0c570e007b85a96e9f886afc825e2981
Lucas Florio <lucasefe@gmail.com>
2010-09-18 14:13:17 -0300

    Making Table act more like an Enumerable.

--- lib/bricks/table.rb
+++ lib/bricks/table.rb
@@ -1,17 +1,37 @@
 module Bricks
   class Table
+    include Enumerable
+    
     attr_accessor :options
     
     def initialize(*args)      
       @options = extract_options!(args)      
+      
       args.first.each do |row|
         rows << row
       end unless args.first.nil?
     end
+    
     def rows
       @rows ||= []
     end
+    
+    def each
+      rows.each do |row|
+        yield(row)
+      end
+    end
+    
+    def empty?
+      rows.empty?
+    end
+    
+    def [](index)
+      rows[index]
+    end
+    
   private
+  
     def extract_options!(args)
       args.last.is_a?(::Hash) ? args.pop : {}
     end
--- spec/bricks/table_spec.rb
+++ spec/bricks/table_spec.rb
@@ -2,28 +2,28 @@ require "spec_helper"
 
 describe Bricks::Table do
   before(:each) do
-    @simple_data = [
+    @data = [
         [ 1, "lucas", "lucasefe@hh.com"]
       ]
     @options = { :header => %w(id name email) }
   end
   it "should be initializable with no arguments" do
     table = Bricks::Table.new
-    table.rows.should be_empty
+    table.should be_empty
     table.options.should == {}
   end
   context "being initialized" do
     it "should be initializable with and array of arrays" do
-      table = Bricks::Table.new(@simple_data)
-      table.rows[0].should == @simple_data[0]
+      table = Bricks::Table.new(@data)
+      table[0].should == @data[0]
     end
     it "should be able to receive only options, with no data" do
       table = Bricks::Table.new(@options)
       table.options.should == @options
     end
     it "should be able to receive data and options" do
-      table = Bricks::Table.new(@simple_data, @options)
-      table.rows[0].should == @simple_data[0]
+      table = Bricks::Table.new(@data, @options)
+      table[0].should == @data[0]
       table.options.should == @options
     end
   end
